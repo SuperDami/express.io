257dea1e4bba9b26c7b11f6932b9845edd315983
superdami <chen.zhejun@rakuraku-inc.com>
2016-03-29 14:02:30 +0900

    require module by name

--- switch.js
+++ switch.js
@@ -2,7 +2,6 @@
 try {
     module.exports = require('./compiled');
 } catch(error) {
-    require('./node_modules/coffee-script');
+    require('coffee-script');
     module.exports = require('./lib');
 }
-
